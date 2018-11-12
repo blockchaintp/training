33b39506cafa2b43c03a021dcc1071b1ea8415c0
Kai Davenport <kaiyadavenport@gmail.com>
2018-11-12 12:07:16 +0800

    page interlinking

--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -8,9 +8,16 @@ import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+
+import IconDescription from '@material-ui/icons/Description'
 
 import snackbarModule from '@nocodesites/redux-modules/lib/modules/snackbar'
 
+import withRouter from '../utils/withRouter'
 import styles from '../styles/document'
 import selectors from '../selectors'
 
@@ -23,7 +30,13 @@ import selectors from '../selectors'
     pages,
   }
 })
+@withRouter()
 class HomePage extends React.Component {
+
+
+  openPage = (url) => {
+    this.props.routerPush(url, null, this.props.router.routesMap)
+  }
 
   render() {
 
@@ -36,6 +49,33 @@ class HomePage extends React.Component {
       <div className={ classes.root }>
         <Paper className={ classes.paper }>
           <Typography variant="title" className={ classes.pageHeader }>Pages</Typography>
+          <List component="nav">
+            <ListItem 
+              button
+              onClick={ () => this.openPage('/') }
+            >
+              <ListItemIcon>
+                <IconDescription />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+            {
+              pages.map((page, i) => {
+                return (
+                  <ListItem 
+                    button 
+                    key={ i }
+                    onClick={ () => this.openPage(page.url) }
+                  >
+                    <ListItemIcon>
+                      <IconDescription />
+                    </ListItemIcon>
+                    <ListItemText primary={ `${page.meta.order}. ${page.meta.title}`} />
+                  </ListItem>
+                )
+              })
+            }
+          </List>
         </Paper>
       </div>
     )
